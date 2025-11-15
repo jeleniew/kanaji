3d178a9de43693e55a2d7974e7a7b50cd2c82821
Wiktoria Jeleniewicz <jeleniewiczwiktoria@gmail.com>
2025-11-15 21:30:58 +0100

    refactor: flashcardView

--- kanaji_app/lib/views/widgets/flashcard_view.dart
+++ kanaji_app/lib/views/widgets/flashcard_view.dart
@@ -6,6 +6,14 @@ class FlashcardView extends StatelessWidget {
   final GestureTapCallback? onTap;
   final GestureDragEndCallback? onHorizontalDragEnd;
 
+  static const int stackCount = 2;
+  static const Color backgroundStackColor = Color(0xFFE0E0E0);
+  static const Color backgroundMainColor = Colors.white;
+  static const Color borderColor = Colors.black;
+  static const double borderWidth = 1.0;
+  static const double borderRadius = 4.0;
+  static const double offsetFactor = 0.03;
+
   const FlashcardView({
     super.key,
     required this.text,
@@ -15,12 +23,12 @@ class FlashcardView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const int stackCount = 2;
 
     return LayoutBuilder(
       builder: (context, constraints) {
-        final double offset = 0.05 * constraints.maxHeight;
+        final double offset = offsetFactor * constraints.maxHeight;
         final mainCardSize = constraints.maxHeight - offset * stackCount;
+        final mainCardOffset = offset * stackCount;
 
         return GestureDetector(
           onTap: onTap,
@@ -29,55 +37,75 @@ class FlashcardView extends StatelessWidget {
             clipBehavior: Clip.none,
             alignment: Alignment.topCenter,
             children: [
-              for (int i = stackCount; i > 0; i--)
-                Positioned(
-                  top: offset * (stackCount - i),
-                  child: Container(
-                    width: mainCardSize - offset * i,
-                    height: mainCardSize - offset * i,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 1,
-                      ),
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                  ),
-                ),
-              Positioned(
-                top: offset * stackCount,
-                child: Container(
-                  width: mainCardSize,
-                  height: mainCardSize,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  alignment: Alignment.center,
-                  child: LayoutBuilder(
-                    builder: (context, constraints) {
-                      final fontSize = constraints.maxHeight * 0.8;
-                      return Text(
-                        text,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: fontSize,
-                          height: 0.5
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ),
+              ..._buildStackedCards(offset, mainCardSize),
+              _buildMainCard(mainCardOffset, mainCardSize, text),
             ],
           ),
         );
       }
+    );
+  }
+
+  TextStyle _textStyle(double fontSize) {
+    return TextStyle(
+      fontSize: fontSize,
+      height: 0.5,
+    );
+  }
+
+  List<Widget> _buildStackedCards(double offset, double mainCardSize) {
+    return List.generate(stackCount, (index) {
+      final layerIndex = stackCount - index;
+      final cardOffset = offset * index;
+      final cardSize = mainCardSize - offset * layerIndex;
+
+      return _card(
+        offset: cardOffset,
+        cardSize: cardSize,
+        backgroundColor: backgroundStackColor,
+      );
+    });
+  }
+
+  Widget _buildMainCard(double offset, double mainCardSize, String text) {
+    return _card(
+      offset: offset,
+      cardSize: mainCardSize,
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final fontSize = constraints.maxHeight * 0.8;
+          return Text(
+            text,
+            textAlign: TextAlign.center,
+            style: _textStyle(fontSize),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _card({
+    required double offset,
+    required double cardSize,
+    Widget? child,
+    Color? backgroundColor,
+  }) {
+    return Positioned(
+      top: offset,
+      child: Container(
+        width: cardSize,
+        height: cardSize,
+        decoration: BoxDecoration(
+          color: backgroundColor ?? backgroundMainColor,
+          border: Border.all(
+            color: borderColor,
+            width: borderWidth,
+          ),
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+        alignment: Alignment.center,
+        child: child,
+      ),
     );
   }
 }
