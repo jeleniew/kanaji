8c0082d40ae36cbea10aa608e4b649254b189a3d
Wiktoria <01178849@pw.edu.pl>
2025-12-05 13:43:51 +0100

    Fix flashcards text size and showing configuration page

--- kanaji_app/lib/data/datasources/route_data_source.dart
+++ kanaji_app/lib/data/datasources/route_data_source.dart
@@ -3,8 +3,8 @@ import 'package:kanaji/domain/entities/app_route.dart';
 class RouteDataSource {
   final List<AppRoute> _routes = [
     AppRoute(name: 'Tracing', path: '/tracing_configuration'),
-    AppRoute(name: 'Flashcards', path: '/flashcards'),
-    AppRoute(name: 'Memory Practice', path: '/memory_practice'),
+    AppRoute(name: 'Flashcards', path: '/flashcards_configuration'),
+    AppRoute(name: 'Memory Practice', path: '/memory_practice_configuration'),
   ];
 
   List<AppRoute> getRoutes() => _routes;
